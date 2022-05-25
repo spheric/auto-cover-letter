93bf1b9acb7ad447822fa8ee15993ef99ae7ce6c
Matthew Gardner <matt.gardner.dev@gmail.com>
2022-05-25 17:51:36 +1000

    Subtle animation to marketing msg

--- app/components/loud_marketing_msg_component.rb
+++ app/components/loud_marketing_msg_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class LoudMarketingMsgComponent < ApplicationComponent
-  def initialize(text: , color: )
+  def initialize(text:, color:, animate:)
     @text = text
     @color = color
+    @animate = animate
   end
 end
--- app/components/loud_marketing_msg_group_component.rb
+++ app/components/loud_marketing_msg_group_component.rb
@@ -2,7 +2,7 @@
 
 class LoudMarketingMsgGroupComponent < ApplicationComponent
   COLORS = {
-    100 => { orange: 'text-orange-100', rose: 'text-rose-100', yellow: 'text-yellow-100' },
+    100 => { orange: 'text-black', rose: 'text-black', yellow: 'text-black' },
     200 => { orange: 'text-orange-200', rose: 'text-rose-200', yellow: 'text-yellow-200' },
     300 => { orange: 'text-orange-300', rose: 'text-rose-300', yellow: 'text-yellow-300' },
     400 => { orange: 'text-orange-400', rose: 'text-rose-400', yellow: 'text-yellow-400' },
@@ -27,12 +27,13 @@ class LoudMarketingMsgGroupComponent < ApplicationComponent
     10 => 'grid-cols-10'
   }
 
-  def initialize(col:, color:, text:, color_gap:)
+  def initialize(col:, color:, text:, color_gap:, animate: true)
     @col = col
     @color = color
     @text = text
+    @animate = animate
 
-    halved_color_range = Array.new(col) { |t| t.zero? ? 100 : t * color_gap + 100 }.slice(0..(col / ( color_gap / 100 ).floor))
+    halved_color_range = Array.new(col) { |t| t.zero? ? 800 : t * color_gap + 100 }.slice(0..(col / ( color_gap / 100 ).floor))
     @start_to_stop_color_range = (halved_color_range << halved_color_range.reverse.drop(1)).flatten
   end
 end
